e2c947b5b9443efc31d1ddbe2ba429048a3ed9fc
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-25 17:45:32 -0300

    bibliotecas

---
+++ app.py
@@ -0,0 +1,5 @@
+import fitz #PyMuPDF
+import nltk #Processamento de linguagem natural
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+from string import punctuation
